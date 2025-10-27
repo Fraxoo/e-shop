263395985fd9e94c87432a17285976f93f876952
Fraxoo <fraxo.officiel@gmail.com>
2025-10-27 16:53:44 +0100

    add header to home page

---
+++ vite-project/src/components/header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <div className="header">
+            <h1>LOGO</h1>
+            <input type="search"></input>
+            <div className="header-right">
+                <p>Account</p>
+                <p>Cart</p>
+            </div>
+        </div>
+    )
+}
--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -1,6 +1,12 @@
+import Header from "../components/header"
+import "../styles/home.css"
+import "../styles/header.css"
+
+
 export default function Home(){
     return(
         <div>
+            <Header />
             <p>hey</p>
         </div>
     )
