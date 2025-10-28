a9e33246ae5822d05c27eba23c599b750b221b54
Fraxoo <fraxo.officiel@gmail.com>
2025-10-28 10:55:09 +0100

    change and redo header

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -1,12 +1,20 @@
-export default function Header(){
+export default function Header() {
     return (
         <div className="header">
-            <h1>LOGO</h1>
-            <input type="search"></input>
-            <div className="header-right">
-                <p>Account</p>
-                <p>Cart</p>
+            <div className="header-top">
+                <h1>e-shop<span id="secondary">.</span></h1>
+                <div className="header-top-right">
+                    <i class="fa-regular fa-user"></i>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
             </div>
+            <div className="header-bottom">
+                <button type="button">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <input type="search" id="search" placeholder="Search Products ..." />
+            </div>
+
         </div>
     )
 }
--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -7,7 +7,6 @@ export default function Home(){
     return(
         <div>
             <Header />
-            <p>hey</p>
         </div>
     )
 }
